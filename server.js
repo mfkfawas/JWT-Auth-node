bb3bac3873d47e0056c334d431e764fe79609297
mfkfawas <mfkfawas@gmail.com>
2022-08-09 22:10:06 +0530

    fn authenticateToken

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 const app = express();
 
-app.get('/posts', (req, res) => {
-  res.json(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name));
 });
 
 app.use(express.json());
@@ -15,11 +16,13 @@ const posts = [
     id: 1,
     title: 'Hello World',
     body: 'This is the first post',
+    username: 'test',
   },
   {
     id: 2,
     title: 'Second Post',
     body: 'This is the second post',
+    username: 'test2',
   },
 ];
 
@@ -33,6 +36,21 @@ app.post('/login', (req, res) => {
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
   res.json({ accessToken });
 });
+
+async function authenticateToken(req, res, next) {
+  let token =
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer') &&
+    req.headers.authorization.split(' ')[1];
+
+  if (!token) return res.status(401).json({ error: 'You are not logged in' });
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: 'You are not logged in' });
+    req.user = user;
+    next();
+  });
+}
 
 const server = app.listen(3000, () => {
   console.log('Server is running on port 3000');
