ea6846a896f3c3eafdf480f530d27b2e6480a467
mfkfawas <mfkfawas@gmail.com>
2022-08-10 13:42:53 +0530

    completed

--- authServer.js
+++ authServer.js
@@ -20,6 +20,28 @@ app.use(express.json());
 //  So really the main reason to use refresh token and so that you can invalidate users that steal access that
 // shouldn't have access.
 // Second reason is that you can take all your authentication and authorization code and move it to a separate server.
+// Normally you would want to store refressh tokens on some form of database or redis cache.
+
+// not a good idea to store refresh tokens in the server. Bcz in prodcn every time your
+// server restarts, the refresh tokens will be emptied out.
+let refreshTokens = [];
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token;
+  if (!refreshToken) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    // 'user' would contain all the decoded data from the refresh token.
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken });
+  });
+});
+
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+  res.sendStatus(204);
+});
 
 app.post('/login', (req, res) => {
   // 1 Authenticate User
@@ -28,9 +50,17 @@ app.post('/login', (req, res) => {
   const username = req.body.username;
   const user = { name: username };
 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken });
+  const accessToken = generateAccessToken(user);
+  // we dont wanna put an exp date on refresh token we're going to manually handle it.
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+
+  res.json({ accessToken, refreshToken });
 });
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s' });
+}
 
 const server = app.listen(4000, () => {
   console.log('Server is running on port 4000');
